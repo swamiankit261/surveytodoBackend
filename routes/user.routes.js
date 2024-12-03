0e99eea265b7111b39d3653c683180a7d58b4f87
swamiankit261 <swamiankit261@gmail.com>
2024-12-03 15:11:30 +0530

    login mathad change

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { loginUser, registerUser, surveyCategory } from '../controllers/user.controller.js';
-import {verifyJWT}  from '../middlewares/auth.middlewares.js';
+import { verifyJWT } from '../middlewares/auth.middlewares.js';
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 
-router.route("/login").get(loginUser);
+router.route("/login").put(loginUser);
 router.route("/mySurvey").put(verifyJWT, surveyCategory);
 
 export default router;
